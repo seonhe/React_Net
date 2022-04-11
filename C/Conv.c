020507b092f4e1b4b07b0e9e038e4697f6f68141
seonhe <osh614@naver.com>
2022-04-11 17:28:00 +0900

    .

---
+++ C/Conv.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// filter (out_channels, in_channels, kernel_size, kernel_size)
+// input (in_channels, in_size, in_size)
+// output (out_channels, (in_size-kernel_size+padding*2)/stride +1, '')
+
+void binary_Conv(int in_channels, int out_channels, int in_size, int kernel_size, int padding, int stride, float* parameter, float* input, float* output){
+    
+}
+
+void real_Conv(int in_channels, int out_channels, int in_size, int kernel_size, int padding, int stride, float* parameter, float* input, float* output){
+
+}
